6898964b5f4a35e6aaa08d4973bc613be590b7bb
we1cheng <10354589+we1cheng@users.noreply.github.com>
2022-05-20 15:37:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall-luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+echo 'src-git pwluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
